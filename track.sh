53d70dc9f232b1ddc1ac9b027d15fb2bd261a418
Santiago Ferreira <san650@gmail.com>
2015-08-24 18:57:45 -0300

    Retrieve 100 items per page

--- track.sh
+++ track.sh
@@ -14,7 +14,10 @@ DATE=$( date "+%Y-%m-%d-%H-%M" )
 FILE_NAME="${DATE}_${USER}_${PROJECT}"
 
 # Download stargazers data
-curl "https://api.github.com/repos/$USER/$PROJECT/stargazers" > "data/$FILE_NAME"
+# FIXME handle pagination https://developer.github.com/v3/#pagination
+curl "https://api.github.com/repos/$USER/$PROJECT/stargazers?per_page=100" > "data/$FILE_NAME"
+# The easiest is to create a couple of requests just in case
+# curl "https://api.github.com/repos/$USER/$PROJECT/stargazers?per_page=100&page=2" >> "data/$FILE_NAME"
 
 if [ $? -ne 0 ]
 then
