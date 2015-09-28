32a881483d833b7d3a0d4c7ba35e5cf7b65ea7dd
Santiago Ferreira <san650@gmail.com>
2015-09-27 22:34:44 -0300

    Generate simple report

---
+++ shortlog.rb
@@ -0,0 +1,65 @@
+#!/usr/bin/env ruby
+
+require "json"
+
+def head
+  <<-EOT
+  <style>
+    a {text-decoration:none}
+    img {width:35px}
+    span {display:none}
+    .deleted{border:1px solid red;opacity:0.5}
+  </style>
+  EOT
+end
+
+def header(file)
+  title = file.gsub(%r{data/(\d+)-(\d+)-(\d+)-.*}, '\3/\2')
+
+  "<h1>#{title}</h1>"
+end
+
+def fragment(user, klass = "")
+  <<-EOT
+    <a href="#{user[2]}" class="user #{klass}" title="@#{user[0]}">
+      <img src="#{user[1]}" alt="#{user[0]}">
+      <span>#{user[0]}</span>
+    </a>
+  EOT
+end
+
+before = []
+current = []
+diff = []
+deleted = []
+
+puts head
+
+Dir["data/*.json"].sort.each do |file|
+  current = []
+
+  JSON.parse(File.read(file)).each do |user|
+    current << [
+      user["login"],
+      user["avatar_url"],
+      user["html_url"]
+    ]
+  end
+
+  diff = current - before
+  deleted = before - current
+  before = current
+
+  next unless (diff.any? or deleted.any?)
+
+  # RENDER
+  puts header(file)
+
+  diff.each do |user|
+    puts fragment(user)
+  end
+
+  deleted.each do |user|
+    puts fragment(user, "deleted")
+  end
+end
